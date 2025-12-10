7f9a82bc91c2cd60ce3e7e8e5245e4a4be7a12ab
Edu <25762736+educhielle@users.noreply.github.com>
2025-12-10 17:18:59 -0300

    tiny experiments

--- inference/main/experiments_ensemble_edclustering.py
+++ inference/main/experiments_ensemble_edclustering.py
@@ -9,10 +9,9 @@ root_models = f"{root}/model_jsons"
 root_datasets = f"{root}/dataset_jsons"
 experiment = "ensemble_ed_clustering"
 program = "ensemble_adapt"
-# compilation_parameters = "LIBRARY=seal"
-compilation_parameters = ""
+compilation_parameters = "LIBRARY=seal"
 sigmas = ["0.0"] # ["0.01", "0.05"]
-ensembles = [2] #, [2, 4, 8]
+ensembles = [2, 4]
 cluster_sizes = [2048, 4096] # [64, 128, 256, 512, 1024, 2048, 4096, 8192]
 
 dlogN = {
--- inference/main/experiments_standard_ensemble.py
+++ inference/main/experiments_standard_ensemble.py
@@ -9,10 +9,9 @@ root_models = f"{root}/model_jsons"
 root_datasets = f"{root}/dataset_jsons"
 experiment = "standard_ensemble"
 program = "ensemble_adapt"
-# compilation_parameters = "LIBRARY=seal CONVOLUTION=classical"
-compilation_parameters = "CONVOLUTION=classical"
+compilation_parameters = "LIBRARY=seal CONVOLUTION=classical"
 sigmas = ["0.0"]
-ensembles = [2] #, [2, 4]
+ensembles = [2, 4]
 
 dlogN = {
     "vgg16"   : "15",
