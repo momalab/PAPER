08e9be53c5c4e19a73da242a7ac9ddc418fe53c7
Edu <25762736+educhielle@users.noreply.github.com>
2025-12-15 22:15:39 -0300

    tiny experiments

--- inference/main/experiments_suite.sh
+++ inference/main/experiments_suite.sh
@@ -2,7 +2,7 @@
 
 # Define arrays
 # programs=("experiments_standard_accuracy.py" "experiments_full_clustering.py" "experiments_slice_clustering.py" "experiments_ensemble_edclustering.py" "experiments_standard_ensemble.py")
-programs=("experiments_ensemble_edclustering.py" "experiments_standard_ensemble.py")
+programs=("experiments_ensemble_edclustering.py")
 # programs=("experiments_standard_accuracy.py")
 models=("resnet32")
 # datasets=("cifar10" "cifar100" "tiny")
