ca8f274bd3a47d387b91ba788d50d11a29f0e368
Edu <25762736+educhielle@users.noreply.github.com>
2025-12-23 10:17:28 -0300

    vgg exp

--- inference/main/experiments_suite.sh
+++ inference/main/experiments_suite.sh
@@ -2,10 +2,13 @@
 
 # Define arrays
 # programs=("experiments_standard_accuracy.py" "experiments_full_clustering.py" "experiments_slice_clustering.py" "experiments_ensemble_edclustering.py" "experiments_standard_ensemble.py")
-programs=("experiments_slice_clustering.py")
-models=("vgg16")
+programs=("experiments_standard_ensemble.py" "experiments_slice_clustering.py" "experiments_ensemble_edclustering.py")
+# models=("vgg16" "resnet18" "resnet20" "resnet32")
 # datasets=("cifar10" "cifar100" "tiny")
-datasets=("cifar10")
+# models=("vgg16")
+# datasets=("cifar10")
+models=("resnet32")
+datasets=("tiny")
 
 # Nested loops
 for dataset in "${datasets[@]}"; do
